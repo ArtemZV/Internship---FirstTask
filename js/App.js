f74f7a8f6de1cd291a224187b9d86f08c0ee7403
ArtemZV <zakharchukartem@gmail.com>
2017-12-14 18:24:20 +0200

    fix

--- js/App.js
+++ js/App.js
@@ -1,36 +1,16 @@
 function App(){
-  this.users = [];
-  this.userFrom = new UserForm(document.getElementById('userForm'));
+  this.emitter = new EventEmitter();
+  this.userFrom = new UserForm(document.getElementById('userForm'), this.emitter);
   this.usersTable = new UsersTable(document.getElementById('usersTable'));  
-  this.reviewForm = new ReviewForm(document.getElementById('reviewForm'));
-  this.init();
+  this.reviewForm = new ReviewForm(document.getElementById('reviewForm'));  
+  this.init()
 }
 
-App.prototype.init = function () {
-  var peter = {name : 'Peter', id: Math.random() * 100},
-      john = {name : 'John', id: Math.random() * 100},
-      tom = {name : 'Tom', id: Math.random() * 100};
-
-  this.users.push(peter, john, tom);
-
-  this.usersTable.emitter.emit('userCreated', peter);
-  this.usersTable.emitter.emit('userCreated', john);
-  this.usersTable.emitter.emit('userCreated', tom);
-  
-  var reviewOnPeter = {reviewText:'Some text', id: Math.random() * 1000, userId: peter.id},
-      reviewOnPeter2 = {reviewText:'Some text2', id: Math.random() * 1000, userId: peter.id},
-      reviewOnJohn = {reviewText:'Lore me text', id: Math.random() * 1000, userId: john.id},
-      reviewOnJohn2 = {reviewText:'short text', id: Math.random() * 1000, userId: john.id},
-      reviewOnTom = {reviewText:'Very very very long text', id: Math.random() * 1000, userId: tom.id},
-      reviewOnTom2 = {reviewText:'unreadable text dsadasd', id: Math.random() * 1000, userId: tom.id};
-
-  this.usersTable.emitter.emit('reviewCreated', reviewOnPeter);
-  this.usersTable.emitter.emit('reviewCreated', reviewOnPeter2);
-  this.usersTable.emitter.emit('reviewCreated', reviewOnJohn);
-  this.usersTable.emitter.emit('reviewCreated', reviewOnJohn2);
-  this.usersTable.emitter.emit('reviewCreated', reviewOnTom);
-  this.usersTable.emitter.emit('reviewCreated', reviewOnTom2);
-  
+App.prototype.init = function(){
+  var self = this;
+  this.emitter.on('newUserCreate', function(user){
+    self.usersTable.emitter.emit('userCreated', user);
+  });
 }
 
 window.addEventListener('DOMContentLoaded', function(){
--- js/UserForm.js
+++ js/UserForm.js
@@ -1,16 +1,17 @@
-function UserForm(el){
+function UserForm(el, emitter){
     if (!el) return;
     this.input = el.querySelector('#nameInput');
     this.btn = el.querySelector('#addUserBtn');
+    this.emitter = emitter;
     this.init();    
 }
 
 UserForm.prototype.init = function () {
-    this.btn.addEventListener('click', this.createUser.bind(this));    
+    this.btn.addEventListener('click', this.createUser.bind(this));   
 }
 
 UserForm.prototype.createUser = function() {
     var name = this.input.value;    
-    if (name == '' || name.match(/^[A-Za-zА-ЯЁа-яё]+$/)) return;
-    new User(name);
+    if (name == '' || !name.match(/^[A-Za-zА-ЯЁа-яё]+$/)) return;
+    this.emitter.emit('newUserCreate', {name: name, id: Math.random() * 100});
 }
