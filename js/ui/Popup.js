99a082ff7a3365c5c3579d8c7b0e48c69f2993bb
ArtemZV <zakharchukartem@gmail.com>
2017-12-18 19:04:36 +0200

    fix

--- js/ui/Popup.js
+++ js/ui/Popup.js
@@ -23,13 +23,21 @@ Popup.prototype.openPopup = function(){
     var iter = setInterval(function(){
         self.popup.style.right = parseFloat(self.popup.style.right) + 50 + "px";
         if (parseFloat(self.popup.style.right) >= 0) {
-            setTimeout(self.closePopup.bind(self), 1000);
+            setTimeout(self.closePopup.bind(self), 1500);
             clearInterval(iter);
         }
     },20);
 }
 
-Popup.prototype.closePopup = function(result){
-    this.popup.style.display = 'none';
-    this.popup.parentNode.removeChild(this.popup)
+Popup.prototype.closePopup = function(){
+    var self = this;
+    self.popup.style.opacity = 1;
+    var iter = setInterval(function(){
+        self.popup.style.opacity = parseFloat(self.popup.style.opacity) - 0.1;
+        if (parseFloat(self.popup.style.opacity) <= 0) {
+            self.popup.style.display = 'none';
+            self.popup.parentNode.removeChild(self.popup)
+            clearInterval(iter);
+        }
+    },60);    
 }
